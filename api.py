0ef6f14b1bd24b9fd7646b19f50bc8bb22b88416
Patrick <89patrick89@gmail.com>
2023-09-26 17:14:49 -0300

    Acrescentado todos os fields necessarios

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ import requests
 from abc import abstractmethod, ABC
 import logging
 import pandas as pd
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -22,8 +23,6 @@ class Rd_api(ABC):
             token = file.read()
 
         return token
-
-    
 
 class GetDeals(Rd_api):
 
@@ -58,26 +57,66 @@ class PutDeals(Rd_api):
         token = self.get_token()
         return f"{self.base_endpoint}/deals/?token={token}"
     
-    def put_data(self,nome_paciente, value_indicacao,data_indicacao, label_indicacao = "64e4c19a2c71dd000e03ead5", label_data_indicacao = "64e4abda173a72001acc99ff", label_fase_lead = "64b13bb2ebfad3000d22bdfe" , **kwargs) -> None:
+    def put_data(self,nome_paciente,telefone,data_nascimento_txt , value_indicacao,data_indicacao, value_sexo,
+                    label_sexo : str = "64e63eac51f84c0018a09ac6", label_indicacao : str = "64e4c19a2c71dd000e03ead5",
+                    label_data_indicacao : str = "64e4abda173a72001acc99ff", label_fase_lead : str = "64b13bb2ebfad3000d22bdfe" , **kwargs) -> None:
         
         endpoint = self._get_endpoint(**kwargs)
-
-
         
+        data_nascimento = datetime.strptime(data_nascimento_txt,('%Y-%m-%d'))
+        dia = data_nascimento.day
+        mes = data_nascimento.month
+        ano = data_nascimento.year
+
+        print (dia,mes,ano)
+
+        self.label_indicacao = label_indicacao
+        self.value_indicacao = value_indicacao
+        self.label_data_indicacao = label_data_indicacao
+        self.nome_paciente = nome_paciente
+        self.telefone = telefone
+        self.data_indicacao = data_indicacao
+        self.value_sexo = value_sexo
+        self.label_sexo = label_sexo
+        self.label_fase_lead = label_fase_lead
+
         payload = { "deal": {
         "deal_custom_fields": [
             {
-                "custom_field_id": f"{self.label_indicacao}",
-                "value": f"{self.value_indicacao}"
+                "custom_field_id": f"{label_indicacao}",
+                "value": f"{value_indicacao}"
             },
             {
-                "custom_field_id": f"{self.label_data_indicacao}",
-                "value": f"{self.data_indicacao}"
+                "custom_field_id": f"{label_data_indicacao}",
+                "value": f"{data_indicacao}"
+            },
+            {
+                "custom_field_id": f"{label_sexo}",
+                "value" : f"{value_sexo}"
             }
         ],
-        "deal_stage_id": f"{self.label_fase_lead}",
-        "name": f"{self.nome_paciente}"
-        } }
+        "deal_stage_id": f"{label_fase_lead}",
+        "name": f"{nome_paciente}"
+        },
+        "contacts": [
+        {
+            "birthday": {
+                "day": dia,
+                "month": mes,
+                "year": ano
+            },
+            "phones": [
+                {
+                    "type": "cellphone",
+                    "phone": f"{telefone}"
+                }
+            ]
+        }
+        ]
+
+
+        }
+        
         headers = {
         "accept": "application/json",
         "content-type": "application/json"
@@ -85,4 +124,4 @@ class PutDeals(Rd_api):
 
         response = requests.post(url=endpoint, json=payload, headers=headers)
 
-        print(response.text)
+        print(response.status_code)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from api import GetDeals
+from api import PutDeals
 
 if __name__ == "__main__":
-    GetDeals().get_data()
+    PutDeals().put_data(nome_paciente='paciente teste api', telefone = '859996716012',data_nascimento_txt='1979-01-01', value_indicacao='Indicacao Teste Completa', data_indicacao='01/01/2023', value_sexo= 'Feminino')
